1a478fb7435f96a50443823fede50e95838e49f0
0BooM <149960425+0BooM@users.noreply.github.com>
2024-07-18 21:45:23 +0200

    Add consolelogBoard(), change blank space in board array to ,,-''
    Created function to log the board like it is in tic-tac-toe to console. Also changed blank spaces in array to hyphen to make it look better in visual

--- scripts.js
+++ scripts.js
@@ -1,17 +1,25 @@
 const Gameboard = (() => {
-  let board = ["", "", "", "", "", "", "", "", ""];
+  let board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
 
   getBoard = () => board;
 
+  consolelogBoard = () => {
+    console.log(`${board[0]} | ${board[1]} | ${board[2]}`);
+    console.log(`${board[3]} | ${board[4]} | ${board[5]}`);
+    console.log(`${board[6]} | ${board[7]} | ${board[8]}`);
+  };
+
   setMark = (index, mark) => {
-    if (board[index] == "") {
+    if (board[index] == "-") {
       board[index] = mark;
       console.log(`You placed ${mark} on square number ${index}`);
     } else console.log(`${index} square is already marked`);
   };
+
   return {
     getBoard,
     setMark,
+    consolelogBoard,
   };
 })();
 
@@ -19,3 +27,4 @@ console.log(Gameboard.getBoard());
 console.log(Gameboard.setMark(1, "X"));
 console.log(Gameboard.setMark(1, "O"));
 console.log(Gameboard.getBoard());
+Gameboard.consolelogBoard();
